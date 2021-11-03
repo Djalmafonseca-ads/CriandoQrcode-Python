3e8809a37198c151eb4b67a4f71b15e8c1fceffd
Djalma Fonseca <76270061+Djalmafonseca-ads@users.noreply.github.com>
2021-11-03 09:17:12 -0300

    Update Criando imagem qrcode.py

--- Criando imagem qrcode.py
+++ Criando imagem qrcode.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
 import qrcode
 imagem_qrcode = qrcode.make("https://www.linkedin.com/in/djalma-neto-dev/")
 imagem_qrcode.save("qrcode_perfil_linkedin.png")
 
 
-# In[ ]:
 
 
 
